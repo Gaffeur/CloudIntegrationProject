95b1e54a3efdcc43aa87bbef010807855b8f37c2
Gaffeur <36623020+Gaffeur@users.noreply.github.com>
2021-12-12 14:28:06 +0100

    init

---
+++ src/main/java/com/project/CloudIntegrationProject/CloudIntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.CloudIntegrationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudIntegrationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudIntegrationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CloudIntegrationProject/CloudIntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CloudIntegrationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudIntegrationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
